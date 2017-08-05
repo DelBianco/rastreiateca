c747d6f708c29d293a90be1c3950dd8befb34dd1
DelBianco <andredbg@gmail.com>
2017-08-05 19:57:38 -0300

    deploy issues

--- app/config/deploy_prod.php
+++ app/config/deploy_prod.php
@@ -15,7 +15,7 @@ return new class extends DefaultDeployer
             ->repositoryUrl('git@github.com:DelBianco/rastreiateca.git')
             // the repository branch to deploy
             ->repositoryBranch('master')
-            ->symfonyEnvironment('prod')
+            ->composerInstallFlags('--no-dev --prefer-dist')
         ;
     }
 
